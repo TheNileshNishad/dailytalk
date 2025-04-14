3ebe61dbcf8142a51c39f863aec773ec7499ee86
Nilesh Kumar Nishad <thenileshnishad@gmail.com>
2025-04-15 01:05:34 +0530

    Implement API endpoints to list non-friends and send friend requests

--- backend/src/controllers/friendController.js
+++ backend/src/controllers/friendController.js
@@ -1,3 +1,8 @@
+import asyncHandler from "../middlewares/asyncHandler.js"
+import mongoose from "mongoose"
+import User from "../models/userModel.js"
+import Friend from "../models/friendModel.js"
+
 const getFriends = (req, res) => {
   res.send("getFriends")
 }
@@ -10,9 +15,61 @@ const getSentRequests = (req, res) => {
   res.send("getSentRequests")
 }
 
-const sendFriendRequest = (req, res) => {
-  res.send("sendFriendRequest")
-}
+const sendFriendRequest = asyncHandler(async (req, res) => {
+  const sender = req.user._id
+  const receiver = req.params.receiverId
+
+  if (!mongoose.Types.ObjectId.isValid(receiver))
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide a valide user ID!" })
+
+  if (sender.toString() === receiver)
+    return res.status(400).json({
+      success: false,
+      message: "You cannot send a friend request to yourself!",
+    })
+
+  const receiverExists = await User.findById(receiver)
+  if (!receiverExists)
+    return res
+      .status(404)
+      .json({ success: false, message: "No user found with that ID!" })
+
+  const existingRequest = await Friend.findOne({
+    $or: [
+      { sender, receiver },
+      { sender: receiver, receiver: sender },
+    ],
+  })
+
+  if (existingRequest) {
+    if (existingRequest.status === "pending") {
+      return res.status(409).json({
+        success: false,
+        message: existingRequest.sender.equals(sender)
+          ? "Friend request already sent!"
+          : "You have a pending request from this user!",
+      })
+    }
+    if (existingRequest.status === "accepted") {
+      return res
+        .status(400)
+        .json({ success: false, message: "You are already friends!" })
+    }
+  }
+
+  const newRequest = await Friend.create({
+    sender,
+    receiver,
+  })
+
+  res.status(201).json({
+    success: true,
+    message: `Friend request sent to ${receiverExists.name}!`,
+    requestId: newRequest._id,
+  })
+})
 
 const cancelFriendRequest = (req, res) => {
   res.send("cancelFriendRequest")
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middlewares/asyncHandler.js"
 import User from "../models/userModel.js"
+import Friend from "../models/friendModel.js"
 
 const getMyProfile = (req, res) => {
   res.status(200).json({
@@ -10,18 +11,18 @@ const getMyProfile = (req, res) => {
 }
 
 const updateMyProfile = asyncHandler(async (req, res) => {
-  const loggedUser = req.user
+  const loggedInUser = req.user
   const { name, userName } = req.body
 
   const alreadyTaken = await User.findOne({ userName })
-  if (alreadyTaken && userName !== loggedUser.userName)
+  if (alreadyTaken && userName !== loggedInUser.userName)
     return res.status(409).json({
       success: false,
       message: "Username is already taken. Please choose another one!",
     })
 
   const user = await User.findByIdAndUpdate(
-    loggedUser._id,
+    loggedInUser._id,
     {
       name,
       userName,
@@ -34,9 +35,34 @@ const updateMyProfile = asyncHandler(async (req, res) => {
     .json({ success: true, message: "Profile updated successfully!", user })
 })
 
-const getAllNonFriendUsers = (req, res) => {
-  res.send("getAllNonFriendUsers")
-}
+const getAllNonFriendUsers = asyncHandler(async (req, res) => {
+  const loggedInUserId = req.user._id
+
+  const friendships = await Friend.find({
+    $or: [{ sender: loggedInUserId }, { receiver: loggedInUserId }],
+  })
+
+  const friendUserIds = friendships
+    .flatMap((friendship) => [
+      friendship.sender.toString(),
+      friendship.receiver.toString(),
+    ])
+    .filter((id) => id !== loggedInUserId.toString())
+
+  const excludedUserIds = [
+    ...new Set([loggedInUserId.toString(), ...friendUserIds]),
+  ]
+
+  const nonFriendUsers = await User.find({
+    _id: { $nin: excludedUserIds },
+  }).select("-password -refreshToken")
+
+  res.status(200).json({
+    success: true,
+    message: "Non-friend users fetched successfully!",
+    nonFriendUsers,
+  })
+})
 
 const searchUsers = (req, res) => {
   res.send("searchUsers")
