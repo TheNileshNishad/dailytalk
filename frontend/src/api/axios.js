5bd4bb8b852625f304b39ff92270c770c9b1d663
Nilesh Kumar Nishad <thenileshnishad@gmail.com>
2025-05-20 14:46:47 +0530

    Add Authorization header with accessToken to axiosInstance

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios"
+import useAuthStore from "../store/authStore"
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  const { accessToken } = useAuthStore.getState()
+  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`
+  return config
 })
 
 export default axiosInstance
