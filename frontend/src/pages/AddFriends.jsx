cf5115e7111edfad8cafac3e6530b62e49196c85
Nilesh Kumar Nishad <thenileshnishad@gmail.com>
2025-05-28 04:10:48 +0530

    Implement friend request sending

--- frontend/src/pages/AddFriends.jsx
+++ frontend/src/pages/AddFriends.jsx
@@ -1,17 +1,34 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import axiosInstance from "../api/axios"
+import { useState } from "react"
 
 const AddFriends = () => {
+  const [activeUserId, setActiveUserId] = useState(null)
+  const queryClient = useQueryClient()
+
+  // get all non-friend users
   const { data: users, isPending } = useQuery({
     queryKey: ["users"],
     queryFn: () => axiosInstance.get("/api/users/search"),
     select: (res) => res.data.users,
   })
 
-  if (isPending) return <p>Pending...</p>
+  // send friend request
+  const sendFriendRequest = useMutation({
+    mutationFn: (userId) =>
+      axiosInstance.post(`/api/friends/requests/${userId}`),
+    onMutate: (userId) => setActiveUserId(userId),
+    onSuccess: (response) => {
+      alert(response.data.message)
+      setActiveUserId(null)
+      queryClient.invalidateQueries({ queryKey: ["users"] })
+    },
+  })
+
+  if (isPending) return <p>Loading users...</p>
 
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 gap-4 justify-items-center">
+    <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 justify-items-center">
       {users.map((user) => (
         <div key={user._id} className="card w-100 bg-base-300 shadow-xl">
           <div className="card-body items-center text-center">
@@ -40,7 +57,13 @@ const AddFriends = () => {
             </p>
             <p className="mt-2">{user?.bio}</p>
             <div className="card-actions mt-4">
-              <button className="btn btn-primary">Send Request</button>
+              <button
+                onClick={() => sendFriendRequest.mutate(user._id)}
+                className="btn btn-primary"
+                disabled={activeUserId === user._id}
+              >
+                {activeUserId === user._id ? "Sending..." : "Send Request"}
+              </button>
             </div>
           </div>
         </div>
